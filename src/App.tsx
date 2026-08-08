6a5766414a2b2bdc04acf6f863f7a56b19cde486
umangjpatel <umangpatel1947@gmail.com>
2026-08-08 12:41:00 -0400

    Initial bento design + ShadCN theming.

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Card } from "@/components/ui/card";
+import {BriefcaseBusiness, Users} from "lucide-react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function WorkExCompanyCard({logoUrl, companyName, jobTitle, duration}: {logoUrl: string, companyName: string, jobTitle: string, duration: string}) {
+    return (
+        <div className="flex gap-4">
+            <img src={`${logoUrl}`} className="size-10 rounded-sm"/>
+            <div className="">
+                <p className="">{companyName}</p>
+                <p className="text-xs">{jobTitle}</p>
+                <p className="text-xs">{duration}</p>
+            </div>
+        </div>
+    )
+}
 
+function WorkExperience() {
+    return (
+        <div className="flex flex-col gap-4">
+            <Card className="p-4">
+                <Users color="orange" className="size-4"></Users>
+                <h2 className="font-bold text-sm">Currently on</h2>
+                <p>Working on Analytics team at Wind River</p>
+            </Card>
+            <Card className="p-4">
+                <BriefcaseBusiness className="size-4" color="skyblue"/>
+                <h2 className="font-bold text-sm">Past</h2>
+                <WorkExCompanyCard
+                    logoUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Kubernetes_logo_without_workmark.svg/960px-Kubernetes_logo_without_workmark.svg.png"
+                    companyName="Ericsson"
+                    jobTitle="Software Developer"
+                    duration="2022-26"
+                />
+                <WorkExCompanyCard
+                    logoUrl="https://images.icon-icons.com/2699/PNG/512/societegenerale_logo_icon_169742.png"
+                    companyName="Societe Generale"
+                    jobTitle="Software Analyst Intern"
+                    duration="2021-22"
+                />
+            </Card>
+        </div>
+    )
+}
+
+function TechStacks() {
+    // TODO: Add content later on
+    return (
+        <div className="flex flex-col gap-4">
+            <Card className="p-4">
+                <BriefcaseBusiness className="size-4" color="skyblue"/>
+                <h2 className="font-bold text-sm">Tech Stacks</h2>
+                <ul>
+                    <li>Kubernetes</li>
+                    <li>Docker</li>
+                    <li>Helm</li>
+                </ul>
+            </Card>
+        </div>
+    )
+}
+
+function BentoGrid() {
+    return (
+        <div className="flex justify-self-center gap-6">
+            <WorkExperience />
+            <TechStacks />
+        </div>
+    )
+}
+
+export function App() {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <div className="p-6">
+      <h1 className="text-center mb-6 text-5xl">Hi, I'm Umang Patel</h1>
+      <BentoGrid />
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  base: "/"
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
